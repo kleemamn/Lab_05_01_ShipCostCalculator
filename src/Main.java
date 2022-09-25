4c9fdbc0e1aabb879348564016fe2a70ae572c88
kleemamn <kleemamn@mail.uc.edu>
2022-09-25 16:49:35 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        double itemPrice = 20.20 ;
+        double shippingTotal ;
+        double grandTotal ;
+        final double FREE_SHIPPING = 100 ;
+        final double SHIPPING_COST_PCT = 0.02 ;
+
+
+        if (itemPrice >= FREE_SHIPPING)
+        {
+            System.out.println("You receive free shipping!");
+        }
+
+
+        
+                
+    }
+}
